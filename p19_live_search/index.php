dd9a37e629d0bb80119ed88c6841785257575c96
MrBaji <mr.baji.smart@gmail.com>
2025-02-28 19:16:56 +0700

    Memodifikasi index.php

--- p19_live_search/index.php
+++ p19_live_search/index.php
@@ -4,7 +4,7 @@ if (!isset($_SESSION["login"])) {
     header("Location: login.php");
     exit;
 }
-require("functions.php");
+require "functions.php";
 $db_cs = query("SELECT * FROM cahaya_surga ORDER BY id DESC");
 
 if (isset($_POST["cari"])) {
