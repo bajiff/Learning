7b0af650041a5467b9ba9c17932027bd270d77c6
MrBaji <mr.baji.smart@gmail.com>
2025-03-06 21:15:56 +0700

    Pengenalan Class & Object

--- PHP OOP/first/index.php
+++ PHP OOP/first/index.php
@@ -1,11 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP OOP</title>
-</head>
-<body>
-    <h1>Belajar PHP OOP</h1>
-</body>
-</html>
+<?php
+class Cobain {
+
+}
+
+$sepatu = new Cobain();
+$sendal = new Cobain();
+var_dump($sepatu);
+var_dump($sendal);
---
+++ PHP OOP/first/products.php
@@ -0,0 +1,9 @@
+<?php
+class Products{
+
+}
+
+$sepatu = new Products();
+$sendal = new Products();
+var_dump($sepatu);
+var_dump($sendal);
