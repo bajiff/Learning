581b19aa73775c2ddb31b7984977952983d50959
MrBaji <mr.baji.smart@gmail.com>
2025-03-18 21:12:25 +0700

    Belajar Routing

--- MVC/baji_2_routing/app/core/App.php
+++ MVC/baji_2_routing/app/core/App.php
@@ -4,6 +4,15 @@ class App
 {
     public function _construct()
     {
-        echo "Selamat Datang Di " . __CLASS__;
+        $url = $this->parseUrl();
+        var_dump($url);
+    }
+    public function parseUrl(){
+        if(isset($_GET["url"])){
+            $url = rtrim($_GET["url"], "/");            return $url;
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode("/", $url);
+            return $url;
+        }
     }
 }
--- MVC/baji_2_routing/index.php
+++ MVC/baji_2_routing/index.php
@@ -4,9 +4,4 @@ use app\core\App as AppCore;
 use app\core\Controller as ControllerCore;
 
 $cobain = new AppCore();
-echo $cobain->_construct();
-
-
-echo "<br>";
-$coba2 = new ControllerCore();
-echo "<br>";
+$cobain->_construct();
